5df40e49b2530145469fec8935f9424fd650a7c5
Ruben Schmidtmann <ruben@rubenschmidtmann.com>
2014-10-10 22:18:15 +0200

    V 1.2
    - latest features (same as workflow)

--- src/new_todoist.sh
+++ src/new_todoist.sh
@@ -2,6 +2,6 @@
 TOKEN=add_your_API_key_here
 
 # Adding task
-QUERY="{query}"
+QUERY=$(python -c "import sys, urllib as ul; print ul.quote_plus('\n'.join(sys.stdin.readlines())[:-1])" <<< "{query}")
 RESP=$(curl --request POST 'https://api.todoist.com/API/addItem' --data "content=$QUERY&priority=1&token=$TOKEN")
 python -c 'import json; import sys; print json.load(sys.stdin)["content"]' <<< "$RESP"
