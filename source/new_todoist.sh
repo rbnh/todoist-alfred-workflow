66c8b0241084ff2923f635cccd7647b856b1c90c
Colin Frei <git@colinfrei.com>
2018-10-28 10:51:38 +0100

    Use v8 of Todoist API
    The current code doesn't seem to use v7 or v8 of the API, and thus didn't work. I changed the query slightly to use the latest version of the API.

--- source/new_todoist.sh
+++ source/new_todoist.sh
@@ -2,6 +2,5 @@
 TOKEN=add_your_API_key_here
 
 # Adding task
-QUERY=$(python -c "import sys, urllib as ul; print ul.quote_plus('\n'.join(sys.stdin.readlines())[:-1])" <<< "{query}")
-RESP=$(curl --request POST 'https://api.todoist.com/API/addItem' --data "content=$QUERY&priority=1&token=$TOKEN")
+RESP=$(curl --request POST 'https://beta.todoist.com/API/v8/tasks' --data '{"content": "{query}"}' -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json")
 python -c 'import json; import sys; print json.load(sys.stdin)["content"]' <<< "$RESP"
