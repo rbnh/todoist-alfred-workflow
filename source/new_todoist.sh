a6f76f5907e42714ac41d401c6b860a429913376
Colin Frei <git@colinfrei.com>
2018-12-10 13:21:28 +0100

    better escaping
    double quotes are escaped by Alfred, but single quotes aren't.
    That'd lead to a single quote in the text breaking the query - this seems to fix it.

--- source/new_todoist.sh
+++ source/new_todoist.sh
@@ -2,5 +2,5 @@
 TOKEN=add_your_API_key_here
 
 # Adding task
-RESP=$(curl --request POST 'https://beta.todoist.com/API/v8/tasks' --data '{"content": "{query}"}' -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json")
+RESP=$(curl --request POST 'https://beta.todoist.com/API/v8/tasks' --data "{\"content\": \"{query}\"}" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json")
 python -c 'import json; import sys; print json.load(sys.stdin)["content"]' <<< "$RESP"
