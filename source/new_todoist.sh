6b6d2013655b888610cb86fa3f11be7260ce160c
Przemysław Rumik <przemelek@gmail.com>
2020-05-10 16:33:52 +0200

    Moved to new API
    It seems that at least for me version with https://api.todoist.com/API/addItem is not working, so I let myself to move this script to newer API

--- source/new_todoist.sh
+++ source/new_todoist.sh
@@ -2,6 +2,14 @@
 TOKEN=add_your_API_key_here
 
 # Adding task
-QUERY=$(python -c "import sys, urllib as ul; print ul.quote_plus('\n'.join(sys.stdin.readlines())[:-1])" <<< "{query}")
-RESP=$(curl --request POST 'https://api.todoist.com/API/addItem' --data "content=$QUERY&priority=1&token=$TOKEN")
-python -c 'import json; import sys; print json.load(sys.stdin)["content"]' <<< "$RESP"
+# first we want to escape all problematic characters like '
+CONTENT=$(python -c "import sys, urllib as ul; print ul.quote_plus('\n'.join(sys.stdin.readlines())[:-1])" <<< "{query}")
+# we need UUID for task....
+UUID=$(python -c "import uuid; print uuid.uuid1()")
+# Lets prepare JSON to add new task :-)
+QUERY='[{"type":"item_add","temp_id":"toster","uuid":"'$UUID'", "args":{"content":"'$CONTENT'"}}]'
+# Send API call to Todoist :-)
+RESP=$(curl -v 'https://api.todoist.com/sync/v8/sync ' -d commands="$QUERY" -d token=$TOKEN)
+# Get answer
+ANS=$(python -c 'import json; import sys; print json.load(sys.stdin)["sync_status"]["'$UUID'"]' <<< "$RESP")
+echo $ANS
