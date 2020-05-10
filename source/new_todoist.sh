f753dead7d9bd7f8bd3e7396d6d44d70919f0861
Przemysław Rumik <przemelek@gmail.com>
2020-05-10 18:17:33 +0200

    Update new_todoist.sh

--- source/new_todoist.sh
+++ source/new_todoist.sh
@@ -2,14 +2,14 @@
 TOKEN=add_your_API_key_here
 
 # Adding task
-# first we want to escape all problematic characters like '
-CONTENT=$(python -c "import sys, urllib as ul; print ul.quote_plus('\n'.join(sys.stdin.readlines())[:-1])" <<< "{query}")
 # we need UUID for task....
 UUID=$(python -c "import uuid; print uuid.uuid1()")
-# Lets prepare JSON to add new task :-)
-QUERY='[{"type":"item_add","temp_id":"toster","uuid":"'$UUID'", "args":{"content":"'$CONTENT'"}}]'
+# first we want to escape all problematic characters like '
+CONTENT=$(python -c "import sys, uuid, json; content=('\n'.join(sys.stdin.readlines())[:-1]); q={'type':'item_add','temp_id':str(uuid.uuid1()),'uuid':'"$UUID"','args':{'content':content}}; print json.dumps(q) " <<< "{query}")
+echo $CONTENT | pbcopy
+QUERY='['$CONTENT']'
 # Send API call to Todoist :-)
 RESP=$(curl -v 'https://api.todoist.com/sync/v8/sync ' -d commands="$QUERY" -d token=$TOKEN)
 # Get answer
-ANS=$(python -c 'import json; import sys; print json.load(sys.stdin)["sync_status"]["'$UUID'"]' <<< "$RESP")
+ANS=$(python -c 'import json; import sys; resp = json.load(sys.stdin).get("sync_status",None); print "Added task: {query}" if resp and resp["'$UUID'"]=="ok" else "Problem with adding new task"' <<< "$RESP")
 echo $ANS
